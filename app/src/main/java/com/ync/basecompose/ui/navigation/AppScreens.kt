f0fffda3dc362540ee003a2536d976765f1c30c5
mvn-ynguyen-dn <y.nguyen@monstar-lab.com>
2022-11-03 14:03:20 +0700

    change name route

--- app/src/main/java/com/ync/basecompose/ui/navigation/AppScreens.kt
+++ app/src/main/java/com/ync/basecompose/ui/navigation/AppScreens.kt
@@ -4,7 +4,7 @@ package com.ync.basecompose.ui.navigation
  * Copyright © Monstarlab Vietnam Co., Ltd.
  * Created by mvn-ynguyen-dn on 11/1/22.
  */
-sealed class AppScreens(var rotate: String) {
+sealed class AppScreens(var route: String) {
     object Splash : AppScreens("splash_screen")
     object Home : AppScreens("home_screen")
 }
