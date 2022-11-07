a39ce97ecfc6ebee0f4037e542bce3a4439d12f6
mvn-ynguyen-dn <y.nguyen@monstar-lab.com>
2022-11-07 10:55:12 +0700

    center value

--- app/src/main/java/com/ync/basecompose/ui/features/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/ync/basecompose/ui/features/favorite/FavoriteScreen.kt
@@ -1,9 +1,11 @@
 package com.ync.basecompose.ui.features.favorite
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.ync.basecompose.ui.components.BaseScreen
 
 /**
@@ -13,7 +15,7 @@ import com.ync.basecompose.ui.components.BaseScreen
 @Composable
 fun FavoriteScreen() {
     BaseScreen {
-        Box(contentAlignment = Alignment.Center) {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
             Text(text = "Favorite Screen")
         }
     }
--- app/src/main/java/com/ync/basecompose/ui/features/search/SearchScreen.kt
+++ app/src/main/java/com/ync/basecompose/ui/features/search/SearchScreen.kt
@@ -1,9 +1,11 @@
 package com.ync.basecompose.ui.features.search
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.ync.basecompose.ui.components.BaseScreen
 
 /**
@@ -13,7 +15,7 @@ import com.ync.basecompose.ui.components.BaseScreen
 @Composable
 fun SearchScreen() {
     BaseScreen {
-        Box(contentAlignment = Alignment.Center) {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
             Text(text = "Search Screen")
         }
     }
